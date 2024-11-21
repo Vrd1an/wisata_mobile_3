553d0dfc9032886bd2369a95557036f2c7d6c553
vrd1an <ferdian.akbar1609@gmail.com>
2024-11-21 09:45:50 +0700

    home page

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:aslab_travel/latihanmodul/Pertemuan4.dart';
 import 'package:aslab_travel/latihanmodul/Pertemuan5.dart';
 import 'package:aslab_travel/latihanmodul/Pertemuan6.dart';
 import 'package:aslab_travel/model/landing_model.dart';
+import 'package:aslab_travel/project/screens/home_page.dart';
 import 'package:aslab_travel/project/screens/landing_page.dart';
 import 'package:aslab_travel/project/screens/splashscreen.dart';
 import 'package:flutter/material.dart';
@@ -16,7 +17,7 @@ class TravelApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: LandingPageOne(),
+      home: HomePage(),
     );
   }
 }
